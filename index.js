17ada5785aa468b8d39f38dead7661781b29e89f
Josh Edwards <jedwards404@gmail.com>
2022-08-08 15:54:12 -0400

    Adding app.use with access crontrol origin allow methods

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const app = express()
 
 /* app.use(express.json()) */
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS')
+        res.send()
+    })
+})
+
 app.use(express.json({ extended: false }))
 
 app.use('/api/', characters)
