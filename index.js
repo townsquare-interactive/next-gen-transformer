eff9e80cbd7f316958ef478f4a0eb5bbd3480d3a
Josh Edwards <jedwards404@gmail.com>
2022-08-08 15:22:35 -0400

    creating characters page with routes in api folder

--- index.js
+++ index.js
@@ -1,69 +1,19 @@
 const express = require('express')
-const app = express()
-const { addOrUpdateCharacter, getCharacters, deleteCharacter, getCharacterById } = require('./dynamo')
+const characters = require('./api/characters')
 
-app.use(express.json())
+const app = express()
+
+/* app.use(express.json()) */
+
+app.use(express.json({ extended: false }))
+
+app.use('/api/', characters)
 
 //each of these are endpoints
 //view localhost:3000/characters
 
 app.get('/', (req, res) => {
-    res.send('Wel come to the Harry Potter character API')
-})
-
-app.get('/characters', async (req, res) => {
-    try {
-        const characters = await getCharacters()
-        res.json(characters)
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({ err: 'Something went wrong' })
-    }
-})
-
-app.get('/characters/:id', async (req, res) => {
-    const id = req.params.id
-    try {
-        const character = await getCharacterById(id)
-        res.json(character)
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({ err: 'Something went wrong' })
-    }
-})
-
-app.post('/characters', async (req, res) => {
-    const character = req.body
-    try {
-        const newCharacter = await addOrUpdateCharacter(character)
-        res.json(newCharacter)
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({ err: 'Something went wrong' })
-    }
-})
-
-app.put('/characters/:id', async (req, res) => {
-    const character = req.body
-    const { id } = req.params
-    character.id = id
-    try {
-        const newCharacter = await addOrUpdateCharacter(character)
-        res.json(newCharacter)
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({ err: 'Something went wrong' })
-    }
-})
-
-app.delete('/characters/:id', async (req, res) => {
-    const { id } = req.params
-    try {
-        res.json(await deleteCharacter(id))
-    } catch (err) {
-        console.error(err)
-        res.status(500).json({ err: 'Something went wrong' })
-    }
+    res.send('Welcome to the Harry Potter character API')
 })
 
 const port = process.env.PORT || 3000
