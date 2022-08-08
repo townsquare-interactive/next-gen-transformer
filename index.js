c589dbdb28d592e893f011455c45b4194962cb40
Josh Edwards <jedwards404@gmail.com>
2022-08-08 16:26:50 -0400

    adding AWS config to index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const routes = require('./api/characters')
+/* const routes = require('./api/characters') */
 const routes = require('./characters')
 
 const app = express()
