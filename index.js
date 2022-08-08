04a3a47d5122fa22ab946b17c8279f28213ff534
Josh Edwards <jedwards404@gmail.com>
2022-08-08 16:06:20 -0400

    Changing characters page to exported function

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const express = require('express')
+const routes = require('./api/characters')
 const characters = require('./api/characters')
 
 const app = express()
 
 /* app.use(express.json()) */
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-
-    // authorized headers for preflight requests
-    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-
-    app.options('*', (req, res) => {
-        // allowed XHR methods
-        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS')
-        res.send()
-    })
-})
-
 app.use(express.json({ extended: false }))
 
-app.use('/api/', characters)
+/* app.use('/api/', characters) */
 
-//each of these are endpoints
-//view localhost:3000/characters
+routes(app)
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Harry Potter character API')
