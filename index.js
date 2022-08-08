91b9dfbf7f8d1a81bbd73062cc45cb0bab75f40c
Josh Edwards <jedwards404@gmail.com>
2022-08-08 16:25:12 -0400

    moving characters outse of api folder

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const routes = require('./api/characters')
-/* const characters = require('./api/characters') */
-const AWS = require('aws-sdk')
-require('dotenv').config()
-
-AWS.config.update({
-    region: process.env.AWS_DEFAULT_REGION,
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-})
+const routes = require('./characters')
 
 const app = express()
 
