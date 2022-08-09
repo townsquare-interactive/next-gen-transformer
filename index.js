c0517457a0da200ee9af109791f7f9473867b091
Josh Edwards <jedwards404@gmail.com>
2022-08-09 14:03:52 -0400

    moving console log

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ app.use(express.json({ extended: true }))
 
 routes(app)
 
-process.env.DYN_DEFAULT_REGION === 'us-east-1' ? console.log('yes') : console.log('no')
-
 app.get('/', (req, res) => {
     res.send('Welcome to the Harry Potter character API')
+    process.env.DYN_DEFAULT_REGION === 'us-east-1' ? console.log('yes') : console.log('no')
 })
 
 const port = process.env.PORT || 3000
