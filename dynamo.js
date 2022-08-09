557ad23e797646b2f859f9cd344d9bbdb0e402d9
Josh Edwards <jedwards404@gmail.com>
2022-08-09 09:28:32 -0400

    adding vercel.json change

--- dynamo.js
+++ dynamo.js
@@ -9,6 +9,7 @@ AWS.config.update({
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient()
 const TABLE_NAME = 'dynamo-api'
+
 const getCharacters = async () => {
     const params = {
         TableName: TABLE_NAME,
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./api/characters')
+require('dotenv').config()
 
 const app = express()
 
