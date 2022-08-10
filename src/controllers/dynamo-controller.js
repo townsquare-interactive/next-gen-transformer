3914ccc1b21d4f6df9471a7aee1ba15a2c68fb07
Josh Edwards <jedwards404@gmail.com>
2022-08-10 15:47:12 -0400

    going back to env vars, to see if I can fix it there

--- src/controllers/dynamo-controller.js
+++ src/controllers/dynamo-controller.js
@@ -2,12 +2,12 @@ const AWS = require('aws-sdk')
 require('dotenv').config()
 
 AWS.config.update({
-    /*     region: process.env.DYN_DEFAULT_REGION,
+    region: process.env.DYN_DEFAULT_REGION,
     accessKeyId: process.env.DYN_ACCESS_KEY_ID,
-    secretAccessKey: process.env.DYN_SECRET_ACCESS_KEY,*/
-    region: 'us-east-1',
+    secretAccessKey: process.env.DYN_SECRET_ACCESS_KEY,
+    /*     region: 'us-east-1',
     accessKeyId: 'AKIA2W2JW4HH3ZOMQBBT',
-    secretAccessKey: '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',
+    secretAccessKey: '5eiB/h4bnlodJMJE55bq1V+qfmRCLxlbal9oD6gv',*/
 })
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient()
