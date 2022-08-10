031f903addeae2d003238547deedbb34770e29ff
Josh Edwards <jedwards404@gmail.com>
2022-08-10 15:42:25 -0400

    using strings instead of env vars

--- src/controllers/dynamo-controller.js
+++ src/controllers/dynamo-controller.js
@@ -2,9 +2,12 @@ const AWS = require('aws-sdk')
 require('dotenv').config()
 
 AWS.config.update({
-    region: process.env.DYN_DEFAULT_REGION,
-    accessKeyId: process.env.NEXT_PUBLIC_DYN_ACCESS_KEY_ID,
-    secretAccessKey: process.env.NEXT_PUBLIC_DYN_SECRET_ACCESS_KEY_ID,
+    /*     region: process.env.DYN_DEFAULT_REGION,
+    accessKeyId: process.env.DYN_ACCESS_KEY_ID,
+    secretAccessKey: process.env.DYN_SECRET_ACCESS_KEY,*/
+    region: 'us-east-1',
+    accessKeyId: 'AKIA2W2JW4HH3ZOMQBBT',
+    secretAccessKey: '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',
 })
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient()
