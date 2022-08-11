380a0104f82fe4888c41cea25a78edb706847e4c
Josh Edwards <jedwards404@gmail.com>
2022-08-11 09:48:59 -0400

    new key changes

--- src/controllers/dynamo-controller.js
+++ src/controllers/dynamo-controller.js
@@ -2,13 +2,13 @@ const AWS = require('aws-sdk')
 require('dotenv').config()
 
 AWS.config.update({
-    /*     region: process.env.DYN_DEFAULT_REGION,
+    region: process.env.DYN_DEFAULT_REGION,
     accessKeyId: process.env.DYN_ACCESS_KEY_ID,
     secretAccessKey: process.env.DYN_SECRET_ACCESS_KEY_ID,
-    signatureVersion: 'v4', */
-    region: 'us-east-1',
-    accessKeyId: 'AKIA2W2JW4HH3ZOMQBBT',
-    secretAccessKey: '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',
+    signatureVersion: 'v4',
+    /*     region: 'us-east-1',
+    accessKeyId: 'AKIA2W2JW4HHXQPE7SDB',
+    secretAccessKey: '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', */
 })
 
 const dynamoClient = new AWS.DynamoDB.DocumentClient()
