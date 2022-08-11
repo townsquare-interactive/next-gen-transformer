cdc577c6cb4f98a84f65d7e2511709b20aa81ec6
Josh Edwards <jedwards404@gmail.com>
2022-08-11 09:55:20 -0400

    cleaning up code after getting it working

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ app.use(express.json({ extended: false }))
 routes(app)
 
 app.get('/', (req, res) => {
-    res.send(`Welcome to the Harry Potter character API ${process.env.DYN_SECRET_ACCESS_KEY_ID === '5eiB/h4bnlodJMJE55bq1V+qfmRCLxlbal9oD6gv' ? 'yes' : 'no'}`)
+    /* res.send(`Welcome to the Harry Potter character API ${process.env.DYN_SECRET_ACCESS_KEY_ID === '5eiB/h4bnlodJMJE55bq1V+qfmRCLxlbal9oD6gv' ? 'yes' : 'no'}`) */
+    res.send(`Welcome to the Harry Potter character API`)
 })
 
 const port = process.env.PORT || 3000
