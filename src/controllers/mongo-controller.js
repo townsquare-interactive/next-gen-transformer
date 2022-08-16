6fca5fba335a3cde0cdf6de034ce01d827e1d99c
Josh Edwards <jedwards404@gmail.com>
2022-08-16 11:30:35 -0400

    cleaning up comments, code

--- src/controllers/mongo-controller.js
+++ src/controllers/mongo-controller.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const Item = require('../../schema')
+const Item = require('../models/schema')
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
