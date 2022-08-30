2e086827cdf0eaf96445f51bf3ebda750735323a
Josh Edwards <jedwards404@gmail.com>
2022-08-30 15:41:58 -0400

    Updatepagelist function confirmed working

--- src/controllers/cms-controller.js
+++ src/controllers/cms-controller.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 const AWS = require('aws-sdk')
+const { S3Client, GetObjectCommand } = require('@aws-sdk/client-s3')
+/* const { getObjectCommand, S3Client } = require('aws-sdk') */
+
 AWS.config.update({
     region: process.env.CMS_DEFAULT_REGION,
     accessKeyId: process.env.CMS_ACCESS_KEY_ID,
@@ -110,6 +113,63 @@ const addPagesS3 = async (pages, data, newUrl) => {
         console.log('Page Placed')
     }
 }
+const addPageS3 = async (page, data) => {
+    let newUrl = stripUrl(data.url)
+    await s3
+        .putObject({
+            Body: JSON.stringify(page),
+            Bucket: 'townsquareinteractive',
+            Key: `${newUrl}/pages/${page.slug}.json`,
+        })
+        .promise()
+
+    console.log('Page Placed')
+}
+
+const transformPageData = function (page) {
+    if (page.backup.data) {
+        page.backup.data.modules = transformCMSMods(page.backup.data.modules)
+    }
+    return page
+}
+
+const updatePageList = async (page, data) => {
+    let newUrl = stripUrl(data.url)
+
+    const params = {
+        Bucket: 'townsquareinteractive',
+        Key: `${newUrl}/pages/page-list.json`,
+    }
+
+    const newFile = await download()
+
+    if (newFile.pages.filter((e) => e.Name === page.title).length === 0) {
+        newFile.pages.push({
+            name: page.title,
+            slug: page.slug,
+            id: page.id,
+            page_type: page.page_type,
+        })
+        console.log(newFile)
+    }
+
+    async function download() {
+        try {
+            // Converted it to async/await syntax just to simplify.
+            const data = await s3.getObject({ Bucket: 'townsquareinteractive', Key: `${newUrl}/pages/page-list.json` }).promise()
+
+            // console.log(data)
+
+            //console.log(data)
+            return JSON.parse(data.Body.toString('utf-8'))
+        } catch (err) {
+            return {
+                statusCode: err.statusCode || 400,
+                body: err.message || JSON.stringify(err.message),
+            }
+        }
+    }
+}
 
 const addSiteDataS3 = async (data) => {
     await s3
@@ -124,4 +184,7 @@ const addSiteDataS3 = async (data) => {
 module.exports = {
     addFilesS3,
     transformCMSData,
+    addPageS3,
+    transformPageData,
+    updatePageList,
 }
--- src/routes/cms-routes.js
+++ src/routes/cms-routes.js
@@ -1,7 +1,7 @@
-const { transformCMSData, addFilesS3 } = require('../controllers/cms-controller')
+const { transformCMSData, addFilesS3, addPageS3, transformPageData, updatePageList } = require('../controllers/cms-controller')
 
 const routes = (app) => {
-    app.post('/pages', async (req, res) => {
+    app.post('/cms', async (req, res) => {
         const newData = transformCMSData(req.body)
 
         try {
@@ -9,6 +9,21 @@ const routes = (app) => {
 
             /* res.json(newData) */
             res.json('All Files added')
+        } catch (err) {
+            console.error(err)
+            res.status(500).json({ err: 'Something went wrong' })
+        }
+    })
+
+    app.post('/page', async (req, res) => {
+        const newPageData = transformPageData(req.body.page)
+        const pageList = updatePageList(req.body.page, req.body.data)
+
+        try {
+            addPageS3(newPageData, req.body.data)
+
+            /* res.json(newData) */
+            res.json(JSON.stringify(pageList))
         } catch (err) {
             console.error(err)
             res.status(500).json({ err: 'Something went wrong' })
