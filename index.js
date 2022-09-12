bd480c8ebd936629e79353d08c81d0f6e44e950d
Josh Edwards <jedwards404@gmail.com>
2022-09-12 12:05:45 -0400

    changing parameter limit

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ app.use((req, res, next) => {
     })
 })
 
-app.use(express.json({ limit: '50mb' }))
-app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }))
+app.use(express.json({ limit: '80mb' }))
+app.use(express.urlencoded({ limit: '80mb', extended: true, parameterLimit: 5000000 }))
 
 routes(app)
 
