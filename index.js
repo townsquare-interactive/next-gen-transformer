a6fd38e296a719682ca153d0b85d0c05dbedc10c
Josh Edwards <jedwards404@gmail.com>
2022-09-19 18:07:21 -0400

    adding headers code back

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const express = require('express')
 const app = express()
 const cms = require('./api/cms-routes')
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+
+    // authorized headers for preflight requests
+    // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+
+    app.options('*', (req, res) => {
+        // allowed XHR methods
+        res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS')
+        res.send()
+    })
+})
+
 app.use(express.json({ limit: '80mb' }))
 app.use(express.urlencoded({ limit: '80mb', extended: false, parameterLimit: 5000000 }))
 
