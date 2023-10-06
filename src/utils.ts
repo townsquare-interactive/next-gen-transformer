1f740e43d1495e4dffb21d6dc2bfe72b735de83f
jedwards4044 <jedwards404@gmail.com>
2023-10-06 15:11:13 -0400

    test vercel deploy

--- src/utils.ts
+++ src/utils.ts
@@ -134,7 +134,6 @@ export const createContactForm = (formTitle: string, email: string) => {
 
             {
                 name: 'email',
-                // placeholder:'Enter Name',
                 type: 'email',
                 label: 'Email',
                 isReq: true,
@@ -144,7 +143,6 @@ export const createContactForm = (formTitle: string, email: string) => {
             },
             {
                 name: 'phone',
-                // placeholder:'Enter Name',
                 type: 'phone',
                 label: 'Phone',
                 isReq: false,
